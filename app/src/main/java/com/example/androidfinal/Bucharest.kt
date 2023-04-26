cacee39d97aa843ed3b57ae55327b23ba3cbd185
lalalaura022 <80111740+lalalaura022@users.noreply.github.com>
2023-04-26 23:10:11 +0300

    Video

--- app/src/main/java/com/example/androidfinal/Bucharest.kt
+++ app/src/main/java/com/example/androidfinal/Bucharest.kt
@@ -1,9 +1,12 @@
 package com.example.androidfinal
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.MediaController
 import android.widget.Toast
+import android.widget.VideoView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidfinal.databinding.ActivityBucharestBinding
@@ -12,11 +15,16 @@ class Bucharest : AppCompatActivity() {
 
     lateinit var binding : ActivityBucharestBinding
     lateinit var toggle: ActionBarDrawerToggle
+    private var myVideoView: VideoView? = null
+    private var myMediaController: MediaController? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBucharestBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        myVideoView = findViewById(R.id.myVideoView)
+        setUpVideoPlayer()
         binding.apply {
             toggle = ActionBarDrawerToggle(this@Bucharest, drawerLayout, R.string.open, R.string.close)
             drawerLayout.addDrawerListener(toggle)
@@ -66,7 +74,28 @@ class Bucharest : AppCompatActivity() {
         }
 
     }
+    private fun setUpVideoPlayer(){
+        if(myMediaController == null){
+            myMediaController = MediaController(this)
+            myMediaController!!.setAnchorView(this.myVideoView)
+        }
+        myVideoView!!.setMediaController(myMediaController)
+        myVideoView!!.setVideoURI(
+            Uri.parse("android.resource://"
+            +packageName + "/" +R.raw.intro)
+        )
+        myVideoView!!.requestFocus()
 
+        myVideoView!!.pause()
+
+        myVideoView!!.setOnCompletionListener {
+            Toast.makeText(applicationContext, "Video Completed", Toast.LENGTH_SHORT).show()
+        }
+        myVideoView!!.setOnErrorListener{ mp, what, extra -> Toast.makeText(applicationContext,"An Error Occured ", Toast.LENGTH_SHORT).show()
+        false
+        }
+
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)){
             true
